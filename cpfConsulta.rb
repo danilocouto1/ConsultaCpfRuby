53acffd6a1e7eeb441b6a5bdcd2b405a0de844b2
Yehior <dansantosalfa@gmail.com>
2023-11-12 14:40:29 -0300

    feat: metodo de ladição do cpf e chamada.

--- cpfConsulta.rb
+++ cpfConsulta.rb
@@ -4,3 +4,14 @@ cpf = 0
 
 puts 'Digite somente os numeros do seu CPF para validação: '
     cpf = gets.chomp.to_i
+
+def validCpf (cpf)
+    if CPF.valid?(cpf)
+        cpf = CPF.new(cpf)
+        puts "O CPF #{cpf.formatted} é valido."
+    else 
+        puts "CPF invalido: #{cpf}!"
+    end
+end
+
+validCpf (cpf)
